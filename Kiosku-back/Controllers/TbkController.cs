e4978d690e16f407a1f1ce71feb0dc79b071dac4
CristinaReyesS <128050395+CristinaReyesS@users.noreply.github.com>
2023-11-23 11:52:03 -0300

    ultimos cambios

--- Kiosku-back/Controllers/TbkController.cs
+++ Kiosku-back/Controllers/TbkController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Kiosku_back.Models;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
@@ -16,41 +17,33 @@ using Transbank.Webpay.TransaccionCompletaMall;
 using Transbank.Webpay.TransaccionCompletaMall.Common;
 using Transbank.Webpay.WebpayPlus;
 
+
 namespace Kiosku_back.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class TbkController : Controller
     {
-        //private readonly IActionContextAccessor _actionContextAccessor;
-        //private readonly IUrlHelperFactory _urlHelperFactory;
-
-        //public TbkController(IUrlHelperFactory urlHelperFactory, IActionContextAccessor actionContextAccessor)
-        //{
-        //    _actionContextAccessor = actionContextAccessor;
-        //    _urlHelperFactory = urlHelperFactory;
-        //}
-
-        public string llamadaWebpay()
+        [HttpPost]
+        [Route("llamadaWebpay")]
+        public IActionResult llamadaWebpay()
         {
             var random = new Random();
             var buyOrder = random.Next(999999999).ToString();
             var sessionId = random.Next(999999999).ToString();
-            var amount = random.Next(1000, 999999);
-            //var urlHelper = _urlHelperFactory.GetUrlHelper(_actionContextAccessor.ActionContext);
-            //var returnUrl = urlHelper.Action("NormalReturn", "WebpayPlus", null, Request.Scheme);
-            var result = (new Transaction()).Create(buyOrder, sessionId, amount, "https://www.google.com");
-
-            //ViewBag.BuyOrder = buyOrder;
-            //ViewBag.SessionId = sessionId;
-            //ViewBag.Amount = amount;
-            ////ViewBag.ReturnUrl = returnUrl;
-            //ViewBag.Result = result;
-            //ViewBag.Action = result.Url;
-            //ViewBag.Token = result.Token;
+            //var amount = random.Next(1000, 999999);
+            var result = (new Transaction()).Create(buyOrder, sessionId, 3600, "http://localhost:4200/login");
 
             string token = Convert.ToString(result.Token);
+            string url = Convert.ToString(result.Url);
 
+            var response = new WebpayResponse
+            {
+                Token = token,
+                Url = url
+            };
 
-            return token;
+            return Ok(response);
 
         } 
 
---
+++ Kiosku-back/Models/WebpayResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Kiosku_back.Models
+{
+    public class WebpayResponse
+    {
+        public string Token { get; set; }
+        public string Url { get; set; }
+    }
+}
